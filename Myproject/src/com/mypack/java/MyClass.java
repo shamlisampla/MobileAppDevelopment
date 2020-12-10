88e7fb34d17d76a4c83846a7d9d36b9a8afdb3b9
shamli <shamlisampla@gmail.com>
2020-12-10 02:05:24 -0500

    first commit

---
+++ Myproject/src/com/mypack/java/MyClass.java
@@ -0,0 +1,14 @@
+package com.mypack.java;
+
+public class MyClass {
+	 int getsum(int x, int y)
+	{
+		return x+y;
+	}
+	
+	public static void main(String[] args)
+	{	MyClass obj = new MyClass();
+		obj.getsum(10,20);
+	}
+
+}
